85c77e6c3f469d5204d6555ef890090ad85b23cf
amitsingh7668 <56405921+amitsingh7668@users.noreply.github.com>
2020-02-25 22:23:31 +0530

    Add files via upload

---
+++ tes-python_schedule.bat
@@ -0,0 +1,4 @@
+:loop
+start "C:\Users\mr_perfect\AppData\Local\Programs\Python\Python37\python.exe" "C:\Users\mr_perfect\AppData\Local\Programs\Python\Python37\Lib\site-packages\testing py\Server_Monitoring.py"
+timeout /t 30
+goto :loop
